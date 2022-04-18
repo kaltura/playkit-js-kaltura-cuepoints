3c260f4c616a189db1f1c9fbe2e94adea6e11423
Serhii Marchenko <Serhii.marchenko@kaltura.com>
2022-04-18 15:25:49 +0300

    fix(FEV-1225): PR comments

--- src/providers/vod/vod-provider.ts
+++ src/providers/vod/vod-provider.ts
@@ -153,7 +153,7 @@ export class VodProvider extends Provider {
         return {
           ...quizQuestionCuePoint,
           startTime,
-          endTime: startTime + 0.5 // quiz cue-point duration 500ms
+          endTime: startTime + 0.5 // quiz cue-point duration 500ms (Safari needs at least 350ms to proper define ative cue-points)
         };
       });
     };
