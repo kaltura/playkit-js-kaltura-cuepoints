b96f8d0be3fc607dd670ea9a9fabfad8aac2679c
JonathanTGold <jonathan.gold@86@gmail.com>
2023-07-24 14:44:35 +0300

    fix(FEC-13283): Centralising cue points manager types duplications (navigations / cuepoints-managers / seo)

---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="./modules" />
+
+export {CuePointManager} from "cuepoint-manager/cuepoint-manager";
+export {CaptionResponse} from "providers/vod/caption-loader";
+export {KalturaThumbCuePoint} from "providers/vod/response-types/kaltura-thumb-cue-point";
+export {KalturaCuePoint} from "providers/vod/response-types/kaltura-cue-point";
+export { CuePoint, KalturaCuePointType, KalturaThumbCuePointSubType, CuePointTags } from "types";
+export {PushNotificationData} from "providers/live/push-notifications-provider";
